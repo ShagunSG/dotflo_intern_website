8825db5558c39afef5b5ba6a42ceba07a60af7ac
ShagunSG <shaguneghatak@gmail.com>
2023-12-26 18:42:42 +0530

    made a navbar

--- snippetsonthego/src/App.js
+++ snippetsonthego/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from './Components/Navbar.js'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <h1>Hello, It's my first React App!</h1>
-       <h2>Happy Coding!!</h2>
+      <h2>Happy Coding!!</h2>
     </div>
   );
 };
