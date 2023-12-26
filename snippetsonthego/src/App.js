ee891b4dbcf463f7c6a0b1ece610ffb03dabc5d7
ShagunSG <shaguneghatak@gmail.com>
2023-12-26 18:48:20 +0530

    created cards.js

--- snippetsonthego/src/App.js
+++ snippetsonthego/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './Components/Navbar.js'
+import Cards from './Components/Cards.js'
 
 const App = () => {
   return (
@@ -7,6 +8,7 @@ const App = () => {
       <Navbar />
       <h1>Hello, It's my first React App!</h1>
       <h2>Happy Coding!!</h2>
+      <Cards />
     </div>
   );
 };
