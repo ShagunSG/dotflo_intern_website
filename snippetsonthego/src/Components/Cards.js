23e6f6e50a7014251d8d2745d8b2e86585b7aef7
ShagunSG <shaguneghatak@gmail.com>
2023-12-27 20:21:42 +0530

    cards made

--- snippetsonthego/src/Components/Cards.js
+++ snippetsonthego/src/Components/Cards.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import logo from '../Images/logo.png'
 import '../App.css'
-// const API = "AIzaSyD50GAaCSUSyNN8pqFMVRbwct4pQo9GLp8";
-// const channelId = 'UC16niRr50-MSBwiO3YDb3RA';
-var fetchurl = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyD50GAaCSUSyNN8pqFMVRbwct4pQo9GLp8&channelId=UC16niRr50-MSBwiO3YDb3RA&part=snippet,id&order=date&maxResults=20';
+var fetchurl = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyD50GAaCSUSyNN8pqFMVRbwct4pQo9GLp8&channelId=UC16niRr50-MSBwiO3YDb3RA&part=snippet,id&order=date&maxResults=21';
 
 export default function Cards() {
 
@@ -17,35 +14,26 @@ export default function Cards() {
             })
     })
 
+    if (!items) {
+        return (
+            <div>
+                <h5>Loading...</h5>
+            </div>
+        )
+    }
     return (
         <div>
             <div className="row row-cols-auto justify-content-center container-fluid">
                 {items.map(item => (
-                    <div className="card col-sm-3 mx-2">
-                        <img src={item.snippet.thumbnails} className="card-img-top" alt="..." />
+                    <div className="card col-sm-3 mx-4 mb-3">
+                        <img src={item.snippet.thumbnails.medium.url} className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">{item.snippet.title}</h5>
                             <p className="card-text">item.</p>
-                            <a href={item.videoLink} className="btn btn-primary">Go somewhere</a>
+                            <a href={item.videoLink} className="btn btn-primary">Watch the Full Video</a>
                         </div>
                     </div>
                 ))}
-                {/* <div className="card col-sm-3 mx-4">
-                    <img src={logo} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-                <div className="card col-sm-3 mx-2">
-                    <img src={logo} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div> */}
             </div>
         </div>
     )
--- snippetsonthego/src/Components/Navbar.js
+++ snippetsonthego/src/Components/Navbar.js
@@ -18,14 +18,7 @@ export default function Navbar() {
                             <li className="nav-item">
                                 <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
-                            </li>
                         </ul>
-                        <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
                     </div>
                 </div>
             </nav>
